f86c1f8f7e823ec1288ff22f6d6ad68ffe627461
ilboued <ilboued@me.com>
2026-04-01 23:29:28 +0200

    Initial Commit

---
+++ galaxy/galaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  galaxyApp.swift
+//  galaxy
+//
+//  Created by Jérôme Binachon on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct galaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
